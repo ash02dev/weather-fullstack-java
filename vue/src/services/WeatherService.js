5f9112a2c64e17325189245e2561bf480c6555c3
Ashley Gills <ashleygills02@gmail.com>
2024-02-01 13:39:18 -0500

    initial push

---
+++ vue/src/services/WeatherService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default {
+    getWeather(zipcode){
+        return axios.get(`/weather?zipcode=${zipcode}`);
+
+    }
+}
---
+++ vue/src/views/WeatherView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <weather-component />
+    </div>
+</template>
+
+<script>
+import WeatherComponent from '../components/WeatherComponent.vue'
+export default {
+    components: { WeatherComponent },
+
+}
+</script>
+
+<style></style>
